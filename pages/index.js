e67609f8c67b25f50d73daf80d8155317fbb44f4
MrAbuz <miguelmrosario@gmail.com>
2022-10-30 01:13:16 +0100

    30/10/2022

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
   //this time we want our button to only exist if we're not connected, so we're gonna add something here called "useState". we need to import it from react
   //useState is different from just "let isConnected = false" because it keeps the state between renders
   const [isConnected, setIsConnected] = useState(false);
-  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
       try {
@@ -25,7 +25,7 @@ export default function Home() {
         let connectedProvider = new ethers.providers.Web3Provider(
           window.ethereum
         );
-        setSigner(connectedProvider.getSigner()); //weird syntax, whats setSigner()?
+        setSigner(connectedProvider.getSigner()); //which will set this to signer because of useState()
       } catch (e) {
         console.log(e);
       }
@@ -34,16 +34,128 @@ export default function Home() {
     }
   }
 
-  async function execute() {}
+  async function execute() {
+    if (typeof window.ethereum !== "undefined") {
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const abi = [
+        {
+          stateMutability: "payable",
+          type: "fallback",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "addPerson",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          name: "nameToFavoriteNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "people",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "favoriteNumber",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "retrieve",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "store",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          stateMutability: "payable",
+          type: "receive",
+        },
+      ];
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store("42");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   //we added a () after return, it just had <div></div>, might be useful dunno
   //its actually pretty simple with {isConnected ? "We are connected" : <button onClick={() => connect()}>Connect!</button>}
   //but the auto format makes it weird and adds some (), but its simple as above. if isConnected is true, it types "We are connected", if not it adds a button
+  //we want our button to only exist if we're not connected
+  //also, we added a react fragment (<> </> there because we added two lines)
   return (
     <div className={styles.container}>
       Hello Frogs!
       {isConnected ? (
-        " We are connected!"
+        <>
+          " We are connected!"
+          <button onClick={() => execute()}>Execute</button>
+        </>
       ) : (
         <button onClick={() => connect()}>Connect!</button>
       )}
